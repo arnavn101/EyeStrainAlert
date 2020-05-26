0ade444a1b4eafa8eb1628d4926f795ab1782825
arnavn101 <arnav.nidumolu@gmail.com>
2020-05-26 11:11:35 -0400

    i

--- Windows/notifier.py
+++ Windows/notifier.py
@@ -9,5 +9,3 @@ class windows_Notifier():
     def show_notification(self, title_notification, message_notification, icon_path, duration_notification):
         self.notifier.show_toast(title_notification, message_notification, icon_path=icon_path, duration=duration_notification)
 
-#windowsNotifications = windows_Notifier()
-#windowsNotifications.show_notification("Healthy Reminder", "Please look away for 10-20 seconds to prevent eye strain",r"app_icon.ico", 10)
